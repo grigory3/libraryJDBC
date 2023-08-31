679b63193b298c3dd45766636ffe36827916629f
grigory <library@project.ru>
2023-08-31 10:55:11 +0300

    Add dependency validator. Add constraints for Person and Book.

--- src/main/java/ru/project/library/models/Person.java
+++ src/main/java/ru/project/library/models/Person.java
@@ -1,10 +1,17 @@
 package ru.project.library.models;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class Person {
     private int id;
 
+    @NotEmpty(message = "Имя не должно быть пустым")
+    @Size(min = 2, max = 100, message = "Имя должно быть от 2 до 100 символов длиной")
     private String fullName;
 
+    @Min(value = 1900, message = "Год рождения должен быть больше, чем 1900")
     private int yearOfBirth;
 
     public Person() {
